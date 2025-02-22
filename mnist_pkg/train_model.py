bc4f4ec64ab5cbe79742b81b5cb516a085281303
John Higgins <jc.higgins@hotmail.co.uk>
2025-02-22 23:36:58 +0000

    Add epochs to train over the data several times

--- mnist_pkg/train_model.py
+++ mnist_pkg/train_model.py
@@ -13,25 +13,27 @@ def train() -> None:
     loss = torch.nn.CrossEntropyLoss()
     running_loss = 0.0
     print_every = 100
+    epochs = 5
     optimiser = torch.optim.Adam(model.parameters(), lr=0.001)
 
     train_loader = get_dataloader()
 
-    for batch_idx, (data, label) in enumerate(train_loader):
-        data = data.to(device)
-        label = label.to(device)
+    for epoch in range(epochs):
+        for batch_idx, (data, label) in enumerate(train_loader):
+            data = data.to(device)
+            label = label.to(device)
 
-        optimiser.zero_grad()
-        output = model(data)
-        loss_value = loss(output, label)
-        running_loss += loss_value
-        loss_value.backward()
-        optimiser.step()
+            optimiser.zero_grad()
+            output = model(data)
+            loss_value = loss(output, label)
+            running_loss += loss_value
+            loss_value.backward()
+            optimiser.step()
 
-        if batch_idx % print_every == 0 and batch_idx > 0:
-            avg_loss = running_loss / print_every
-            print(f'Batch {batch_idx}, Average Loss: {avg_loss:.4f}')
-            running_loss = 0.0
+            if batch_idx % print_every == 0 and batch_idx > 0:
+                avg_loss = running_loss / print_every
+                print(f'Epoch {epoch+1}/{epochs}, Batch {batch_idx}, Average Loss: {avg_loss:.4f}')
+                running_loss = 0.0
 
 
 if __name__ == "__main__":
