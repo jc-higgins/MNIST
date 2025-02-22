bad175882efd780b90e395bed41b307fe1293c24
John Higgins <jc.higgins@hotmail.co.uk>
2025-02-22 23:34:34 +0000

    Add running average to make loss smooth

--- mnist_pkg/train_model.py
+++ mnist_pkg/train_model.py
@@ -11,21 +11,27 @@ def train() -> None:
 
     # Create Loss function and Optimiser
     loss = torch.nn.CrossEntropyLoss()
+    running_loss = 0.0
+    print_every = 100
     optimiser = torch.optim.Adam(model.parameters(), lr=0.001)
 
     train_loader = get_dataloader()
 
-    for data, label in train_loader:
+    for batch_idx, (data, label) in enumerate(train_loader):
         data = data.to(device)
         label = label.to(device)
 
         optimiser.zero_grad()
         output = model(data)
         loss_value = loss(output, label)
+        running_loss += loss_value
         loss_value.backward()
         optimiser.step()
 
-        print(f"Loss: {loss_value.item():.4f}")
+        if batch_idx % print_every == 0 and batch_idx > 0:
+            avg_loss = running_loss / print_every
+            print(f'Batch {batch_idx}, Average Loss: {avg_loss:.4f}')
+            running_loss = 0.0
 
 
 if __name__ == "__main__":
